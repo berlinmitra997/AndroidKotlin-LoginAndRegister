c6ffd0e4f16693a1f63254d41f9c4c78356abfaa
Berlin Mitra Putra <berlinmitra997@gmail.com>
2018-07-30 22:29:04 +0700

    First Commit

---
+++ app/src/main/java/com/example/berlin/logregkotlin/Model/User.kt
@@ -0,0 +1,6 @@
+package com.example.berlin.logregkotlin.Model
+
+/**
+ * Created by Berlin on 30/07/2018.
+ */
+data class User(val id: Int = -1, val name: String, val email: String, val password: String)
